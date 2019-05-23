06dc48ce1074b23c4d5f489eb2172ba98690fb0f
FabienNeibaf <fabienlintone@gmail.com>
2019-05-23 17:10:44 +0300

    Build Form with tag helper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_131839) do
+ActiveRecord::Schema.define(version: 2019_05_23_131815) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
